c6752ed4e5c2d0a01c962faef896a3450fe8b132
cornicheli <gabrielcornide.pg@gmail.com>
2022-10-07 03:53:34 -0300

    task2js

---
+++ javascript/index.js
@@ -0,0 +1,18 @@
+let contenedorHTML = document.getElementById('tarjetero')
+
+function printcard(contenedor){
+    for(let evento of events){
+        contenedor.innerHTML +=`
+        <section class="card">
+            <img class="img-card" src="${evento.image}" alt="foto">
+            <h2>${evento.name}</h2>
+            <p>${evento.description}</p>
+            <div class="texto">
+                <p>${evento.price}</p>
+                <button>Read More</button>
+            </div>
+        </section>
+        `
+    }
+}
+printcard(contenedorHTML)
---
+++ javascript/upcoming-events.js
@@ -0,0 +1,19 @@
+let contenedorHTML = document.getElementById("tarjetero");
+
+function printcard(contenedor) {
+  for (let evento of events) {
+    if (evento.date > currentDate)
+      contenedor.innerHTML += `
+        <section class="card">
+            <img class="img-card" src="${evento.image}" alt="foto">
+            <h2>${evento.name}</h2>
+            <p>${evento.description}</p>
+            <div class="texto">
+                <p>${evento.price}</p>
+                <button>Read More</button>
+            </div>
+        </section>
+        `;
+  }
+}
+printcard(contenedorHTML);
